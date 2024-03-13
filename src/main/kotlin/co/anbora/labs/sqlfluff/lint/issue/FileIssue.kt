61b48384e4893276b1d4ee618a677aa78649f65c
Darwin Algarin <dalgarins@live.com>
2024-03-12 21:20:30 -0500

    add compatibility with sqlfluff 3.0.0

--- src/main/kotlin/co/anbora/labs/sqlfluff/lint/issue/FileIssue.kt
+++ src/main/kotlin/co/anbora/labs/sqlfluff/lint/issue/FileIssue.kt
@@ -1,5 +1,8 @@
 package co.anbora.labs.sqlfluff.lint.issue
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class FileIssue(
     var filepath: String?,
     var violations: List<IssueItem>?
--- src/main/kotlin/co/anbora/labs/sqlfluff/lint/issue/IssueItem.kt
+++ src/main/kotlin/co/anbora/labs/sqlfluff/lint/issue/IssueItem.kt
@@ -1,11 +1,16 @@
 package co.anbora.labs.sqlfluff.lint.issue
 
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class IssueItem(
     @JsonProperty("line_no")
+    @JsonAlias("start_line_no")
     var lineNo: Int?,
     @JsonProperty("line_pos")
+    @JsonAlias("start_line_pos")
     var linePos: Int?,
     @JsonProperty("code")
     var code: String?,
